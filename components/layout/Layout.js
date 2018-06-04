a21df497afa4792663cc5174c2e0837bf1618d58
Renan <renan@vedati.com.br>
2018-06-03 22:40:35 -0300

    Created Navbar component

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Container } from 'semantic-ui-react'
+import Navbar from './Navbar'
 
 
 export default class Layout extends React.Component {
     render () {
         return (
             <React.Fragment>
+                <Navbar/>
                 <Container>
                     {this.props.children}
                 </Container>
---
+++ components/layout/Navbar.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Link from 'next/link'
+import { Menu } from 'semantic-ui-react'
+import { withRouter } from 'next/router'
+
+
+class Navbar extends React.Component {
+    render () {
+        return (
+            <Menu stackable id='navbar' style={{borderRadius: 0}}>
+                <Menu.Item header style={{fontFamily: 'Encode Sans', paddingTop: '11px', paddingBottom: '11px', fontSize: '18px'}}>
+                    <span style={{color: '#FF9800'}}>Veda Wiki</span>
+                </Menu.Item>
+
+                <Link href='/'>
+                    <Menu.Item as="a" href='/' active={this.props.router.pathname === '/'}>News</Menu.Item>
+                </Link>
+
+                <Link href='/sets'>
+                    <Menu.Item as="a" href='/sets' active={this.props.router.pathname.includes('/sets')}>Sets</Menu.Item>
+                </Link>
+
+                <Menu.Menu position='right'>
+                    <Menu.Item>
+                    </Menu.Item>
+                </Menu.Menu>
+            </Menu>
+        )
+    }
+}
+
+export default withRouter(Navbar)
