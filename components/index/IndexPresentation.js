b5ac7c7147088d916455f4b6e5682e3c50aeb5a9
Renan <renan@vedati.com.br>
2018-06-05 01:56:17 -0300

    Created IndexPresentation component

---
+++ components/index/IndexPresentation.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Title from '../shared/Title'
+
+
+export default class IndexPresentation extends React.PureComponent {
+    render () {
+        return (
+            <React.Fragment>
+                <Title>Presentation</Title>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum in lacinia odio, ut hendrerit libero. Morbi at neque a leo tincidunt tempus. Nulla vitae velit et ante efficitur efficitur eget vel lacus. Aliquam aliquet enim vitae magna sollicitudin pharetra. Nullam consequat vel leo ac eleifend. Aenean lacinia eget risus eget suscipit. Cras nec tellus ut arcu vehicula ultricies. Integer quis finibus diam. Vestibulum eget turpis sit amet diam condimentum mollis. Nulla viverra elementum arcu, id ornare purus rhoncus in. Nam sed eleifend quam. Donec non risus et orci commodo ultricies. Quisque orci ligula, mattis quis pharetra sed, aliquam luctus mi.</p>
+            </React.Fragment>
+        )
+    }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,21 @@
 import React from 'react'
+import { Grid } from 'semantic-ui-react'
 import Layout from '../components/layout/Layout'
-import Title from '../components/shared/Title'
+import IndexPresentation from '../components/index/IndexPresentation'
 
 
 export default class Index extends React.Component {
     render () {
         return (
             <Layout>
-                <Title>Index</Title>
+                <Grid reversed='mobile' stackable  columns={2}>
+                    <Grid.Column>
+                        <IndexPresentation/>
+                    </Grid.Column>
+                    <Grid.Column>
+                        login
+                    </Grid.Column>
+                </Grid>
             </Layout>
         )
     }
